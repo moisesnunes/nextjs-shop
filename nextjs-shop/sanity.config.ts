8d480daf1aa7b836c8964b385df4123d150e4200
Moises Nunes <moisesnunes10086@gmail.com>
2023-08-30 13:34:22 -0300

    error

--- nextjs-shop/sanity.config.ts
+++ nextjs-shop/sanity.config.ts
@@ -1,6 +1,5 @@
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
 export default defineConfig({
@@ -10,7 +9,7 @@ export default defineConfig({
   projectId: 'p4k9cmtb',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool()],
 
   schema: {
     types: schemaTypes,
