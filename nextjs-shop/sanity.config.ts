206219087fc0aeae89b6869e9b18531b83eeba26
Moises Nunes <moisesnunes10086@gmail.com>
2023-08-30 13:52:55 -0300

    arrumando erro

--- nextjs-shop/sanity.config.ts
+++ nextjs-shop/sanity.config.ts
@@ -1,5 +1,6 @@
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
 export default defineConfig({
@@ -9,7 +10,7 @@ export default defineConfig({
   projectId: 'p4k9cmtb',
   dataset: 'production',
 
-  plugins: [deskTool()],
+  plugins: [deskTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
